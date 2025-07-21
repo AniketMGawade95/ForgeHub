105055cd47c71d217cc0c122debfeef36cc88e44
AniketMGawade95 <aniketmgawade2019@gmail.com>
2025-07-21 09:23:37 +0530

    dto

--- ForgeHubApi/ForgeHubApi/DTO/RFQDto.cs
+++ ForgeHubApi/ForgeHubApi/DTO/RFQDto.cs
@@ -1,14 +1,38 @@
-﻿namespace ForgeHubApi.DTO
+﻿using ForgeHubApi.Models;
+using ForgeHubProj.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ForgeHubApi.DTO
 {
-    public class RFQDto
+    public class RFQDTO
     {
+       
         public int RFQId { get; set; }
+
         public string RFQNo { get; set; }
         public string IndentNo { get; set; }
+        public string? RFQLineNo { get; set; }
+        public string? ItemNo { get; set; }
         public string? ItemName { get; set; }
         public int? ReqQty { get; set; }
+        public string? Description { get; set; }
+        public string? DeliveryLocation { get; set; }
+        public string? UOM { get; set; }
         public DateTime? ReqDeliveryDate { get; set; }
-        public string? Status { get; set; }
-        public string BuyerEmail { get; set; } // From Users
+        public string? FactoryCode { get; set; }
+        public DateTime? BidDate { get; set; } = DateTime.Now;
+        public DateTime? ExpiryDateofBid { get; set; }
+
+       
+        public int BuyerId { get; set; }
+   
+
+        public string? Mobile { get; set; }
+        public string? ContactPerson { get; set; }
+
+
+        public string? Status { get; set; } = "Open";
+
     }
 }
